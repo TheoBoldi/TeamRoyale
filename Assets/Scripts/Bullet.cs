3961c8a4ffd5026a73002de8b1d572dab7443339
Camille <camille.lavarde@gmail.com>
2020-09-22 10:49:05 +0200

    rectif bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(gameObject);
     }
 }
