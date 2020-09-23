b32b381691e2b74e50a637d7e8009c2a22f1b0f4
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-09-23 14:02:05 +0200

    Shield power is finished.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public int damage = 10;
+    public int damage = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isTrigger = false;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("bullet"))
+            isTrigger = true;
+    }
+}
