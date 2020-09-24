17ca0e767c2ae6537a9ccda19d64389786ff4f37
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-09-24 13:55:06 +0200

    fix bug when the player catch an object. fix some prefabs values.

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,10 @@ public class Object : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && collision.GetType() == typeof(UnityEngine.CapsuleCollider2D))
+        {
+            collision.GetComponent<PlayerGoals>().objectsCollected++;
             Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerGoals.cs
+++ Assets/Scripts/PlayerGoals.cs
@@ -6,15 +6,13 @@ public class PlayerGoals : MonoBehaviour
 {
     [HideInInspector]
     public bool levelFinished = false;
-
-    private int objectsCollected = 0;
+    [HideInInspector]
+    public int objectsCollected = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Finish")
             && GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().objects.Length <= objectsCollected)
             levelFinished = true;
-        else if (collision.CompareTag("Object"))
-            objectsCollected++;
     }
 }
