f25af5d4e4bd8e5d621cb8029fdb132a76755239
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-09-24 11:03:18 +0200

    - Add obstacle prefab. - Fix collision with obstacle. - Add plugin for transition.

---
+++ Assets/Scripts/Menus/TransitionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TransitionController : MonoBehaviour
+{
+    public static TransitionController instance = null;
+
+    public Image transitionImage = null;
+    public float timeToTransition = 1;
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+        else if (instance != this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void FadeIn(Action callback)
+    {
+        transitionImage.DOFade(1, timeToTransition / 2).SetUpdate(true).OnComplete(()=> callback());
+    }
+
+    public void FadeOut(Action callback = null)
+    {
+        transitionImage.DOFade(0, timeToTransition / 2).SetUpdate(true).OnComplete(() => callback?.Invoke());
+    }
+}
